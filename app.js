f6c012907df6ed0065c7dd4a01aa52f86e31958f
kimyb <bruce0302@naver.com>
2019-11-19 05:32:17 +0900

    app2, package.json

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-var express = require('express');
-var http = require('http');
+var express = require('express'),
+    http = require('http');
 
+// 익스프레스 객체 생성
+var app = express();
+
+// 기본 포트를 app 객체에 속성으로 설정
+app.set('port', process.env.PORT || 3000);
+
+// EXPRESS 서버 시작
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('익스프레스 서버를 시작했습니다 : ' + app.get('port'));
+});
